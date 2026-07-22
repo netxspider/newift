58508d4bc47a44510c665149bf9e76da3c2588c8
Arnav Raj <arnavraj@Arnavs-MacBook-Air-2.local>
2026-07-22 22:03:55 +0530

    Add Google site verification

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en" data-theme="light">
       <head>
         <Script id="google-tag-manager" strategy="beforeInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KHNHD7DS');`}</Script>
+        <meta name="google-site-verification" content="b7Ibb-CurAzWO4WPTgf-8-9oXrcBSzSO5wbmrnf_NwY" />
         <meta name="google-adsense-account" content="ca-pub-9668645091104713" />
         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9668645091104713" crossOrigin="anonymous" strategy="afterInteractive" />
       </head>
